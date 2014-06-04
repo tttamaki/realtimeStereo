2d7a36063545543723bdee3e310ab89be6771f19
Toru Tamaki <tamaki@hiroshima-u.ac.jp>
2014-06-04 11:40:40 +0900

    clean code futher
    pointer is used for stereoBM instead of instance

--- main.cpp
+++ main.cpp
@@ -1,15 +1,18 @@
+//!<
+//!< OpenCV version > git clone 2014/Jun/03
+//!<
+
 #include <iostream>
+#include <vector>
 
-#include <cv.h>
-#include <highgui.h>
+#include <cv.hpp> //!< OpenCV C++ class. not "cv.h" which is for C.
 
-#include <FlyCapture2.h>
+#include <FlyCapture2.h> //!< Point Grey SDK FlyCapture2 version == 2.6.3.2
 
-#include "elas.h"
-
-
+#include "elas.h" //!< Geiger's ACCV2010 paper implementation
 
 #include <boost/thread/thread.hpp>
+
 #include <pcl/common/common_headers.h>
 #include <pcl/io/pcd_io.h>
 #include <pcl/visualization/pcl_visualizer.h>
@@ -27,7 +30,7 @@
 //! \param FlyCapture2::Error error
 //!
 //!
-void PrintError( FlyCapture2::Error error )
+void PrintError( const FlyCapture2::Error error )
 {
     error.PrintErrorTrace();
 }
@@ -37,7 +40,7 @@ void PrintError( FlyCapture2::Error error )
 if (error != FlyCapture2::PGRERROR_OK) \
 { \
     PrintError( error ); \
-    return -1; \
+    exit(-1); \
 }
 
 #define PRINT_ERROR_MSG( error, msg ) \
@@ -46,7 +49,7 @@ if (error != FlyCapture2::PGRERROR_OK) \
     PrintError( error ); \
     std::cerr << msg << std::endl; \
     getchar(); \
-    return -1; \
+    exit(-1); \
 }
 
 
@@ -56,7 +59,7 @@ if (error != FlyCapture2::PGRERROR_OK) \
 //! \param FlyCapture2::CameraInfo* pCamInfo
 //!
 //!
-void PrintCameraInfo( FlyCapture2::CameraInfo* pCamInfo )
+void PrintCameraInfo( const FlyCapture2::CameraInfo* pCamInfo )
 {
     std::cout <<
               "*** CAMERA INFORMATION ***" << std::endl <<
@@ -73,11 +76,10 @@ void PrintCameraInfo( FlyCapture2::CameraInfo* pCamInfo )
 
 //! \brief load calibration info used by cv::remap()
 //!
-//! \param cv::Mat& mx1
-//! \param cv::Mat& my1
-//! \param cv::Mat& mx2
-//! \param cv::Mat& my2
-//! \return mx1, my1, mx2, my2
+//! \return cv::Mat& mx1
+//! \return cv::Mat& my1
+//! \return cv::Mat& mx2
+//! \return cv::Mat& my2
 //!
 //!
 void loadCalibrationInfo(cv::Mat& mx1,
@@ -101,27 +103,25 @@ void loadCalibrationInfo(cv::Mat& mx1,
 
 
 
-int main( int /*argc*/, char** /*argv*/ )
+//! \brief Connect cameras and return camera objects
+//!
+//! \return unsigned int &numCameras
+//! \return FlyCapture2::Camera** &ppCameras
+//!
+//!
+void prepareCameras(unsigned int &numCameras,
+                    FlyCapture2::Camera** &ppCameras)
 {
-    const int k_numImages = 100000;
+
     FlyCapture2::Error error;
 
-    cv::Mat Q, M1, D1, R1, P1, M2, D2, R2, P2;
 
-
-
-    cv::Mat mx1, my1, mx2, my2;
-    loadCalibrationInfo(mx1, my1, mx2, my2);
-
-
-
-
-
-
-
+    //!<
+    //!< Get camera bus
+    //!<
 
     FlyCapture2::BusManager busMgr;
-    unsigned int numCameras;
+
     error = busMgr.GetNumOfCameras( &numCameras );
     PRINT_ERROR( error );
     std::cout << "Number of cameras detected: " << numCameras << std::endl;
@@ -130,16 +130,18 @@ int main( int /*argc*/, char** /*argv*/ )
     {
         std::cerr << "Insufficient number of cameras... press Enter to exit." << std::endl;
         getchar();
-        return -1;
+        exit(-1);
     }
 
 
 
-    FlyCapture2::Camera** ppCameras = new FlyCapture2::Camera* [numCameras];
+    ppCameras = new FlyCapture2::Camera* [numCameras];
 
 
-    // Connect to all detected cameras and attempt to set them to
-    // a common video mode and frame rate
+    //!<
+    //!< Connect to all detected cameras and attempt to set them to a common video mode and frame rate
+    //!<
+
     for ( unsigned int i = 0; i < numCameras; i++)
     {
         ppCameras[i] = new FlyCapture2::Camera();
@@ -156,59 +158,102 @@ int main( int /*argc*/, char** /*argv*/ )
 
 
         //!< Get the camera information
-        FlyCapture2::CameraInfo camInfo;
-        error = ppCameras[i]->GetCameraInfo( &camInfo );
-        PRINT_ERROR( error );
+        {
+            FlyCapture2::CameraInfo camInfo;
+
+            error = ppCameras[i]->GetCameraInfo( &camInfo );
+            PRINT_ERROR( error );
+
+            PrintCameraInfo( &camInfo );
+        }
 
 
-        PrintCameraInfo( &camInfo );
 
-        // Set all cameras to a specific mode and frame rate so they
-        // can be synchronized
+        //!< Set all cameras to a specific mode and frame rate so they can be synchronized
         error = ppCameras[i]->SetVideoModeAndFrameRate(FlyCapture2::VIDEOMODE_640x480Y8,
-        FlyCapture2::FRAMERATE_60 );
+                                                       FlyCapture2::FRAMERATE_60 );
         PRINT_ERROR_MSG( error,
-        "Error starting cameras. \n"
-        "This example requires cameras to be able to set to 640x480 Y8 at 30fps. \n"
-        "If your camera does not support this mode, please edit the source code and recompile the application. \n"
-        "Press Enter to exit. \n");
+                        "Error starting cameras. \n"
+                        "This example requires cameras to be able to set to 640x480 Y8 at 30fps. \n"
+                        "If your camera does not support this mode, please edit the source code and recompile the application. \n"
+                        "Press Enter to exit. \n");
     }
 
 
-    std::cout << "all camera setted to specific mode." << std::endl;
+    std::cout << "all camera set to specific mode." << std::endl;
 
 
     error = FlyCapture2::Camera::StartSyncCapture( numCameras, (const FlyCapture2::Camera**)ppCameras );
     PRINT_ERROR_MSG( error,
-    "Error starting cameras. \n"
-    "This example requires cameras to be able to set to 640x480 Y8 at 30fps. \n"
-    "If your camera does not support this mode, please edit the source code and recompile the application. \n"
-    "Press Enter to exit. \n");
-
+                    "Error starting cameras. \n"
+                    "This example requires cameras to be able to set to 640x480 Y8 at 30fps. \n"
+                    "If your camera does not support this mode, please edit the source code and recompile the application. \n"
+                    "Press Enter to exit. \n");
 
     std::cout << "StartSyncCapture." << std::endl;
 
+}
+
+
+int main( int /*argc*/, char** /*argv*/ )
+{
 
 
 
-//  cv::Mat imageCamera(rawImage.GetRows(), rawImage.GetCols(), CV_8UC1);
+
+
+    //!< OpenCV control panel: block stereo matching parameters
+    cv::namedWindow("control panel", cv::WINDOW_NORMAL );
+    int ndisparities = 5;
+    cv::createTrackbar( "(n+1)x16=ndisparities", "control panel", &ndisparities, 20, NULL ); //!< ndisparities % 16 == 0, positive, ndisparities < width
+    int blockSize = 3;
+    cv::createTrackbar( "(s+2)x2+1=blockSize", "control panel", &blockSize, 30, NULL ); //!<  5 <= blocksize <= 255, odd
+
+
+
+
+
+    //!< load camera calibration info
+    cv::Mat mx1, my1, mx2, my2;
+    loadCalibrationInfo(mx1, my1, mx2, my2);
+
+
+    //!< connect to cameras
+    unsigned int numCameras;
+    FlyCapture2::Camera** ppCameras;
+    prepareCameras( numCameras, ppCameras );
+
+
+
+
+
+    //!< allocate images
     cv::Mat imageCamera0(480, 640, CV_8UC1);
     cv::Mat imageCamera1(480, 640, CV_8UC1);
+    std::vector< cv::Mat* > pimageCamera;
+    pimageCamera.push_back( &imageCamera0 );
+    pimageCamera.push_back( &imageCamera1 );
+
     cv::Mat imageDisparity(480, 640, CV_32F), disp8U;
-    cv::Mat* pimageCamera[]= {&imageCamera0, &imageCamera1};
-    cv::namedWindow( "cam0", CV_WINDOW_AUTOSIZE );
-    cv::namedWindow( "cam1", CV_WINDOW_AUTOSIZE );
-    cv::namedWindow( "disparity", CV_WINDOW_AUTOSIZE );
+
+    cv::namedWindow( "cam0", cv::WINDOW_AUTOSIZE );
+    cv::namedWindow( "cam1", cv::WINDOW_AUTOSIZE );
+    cv::namedWindow( "disparity", cv::WINDOW_AUTOSIZE );
+
+
+
 #ifdef USE_OPENCV
-    int ndisparities = 128;
-    cv::StereoBM SBM(cv::StereoBM::NARROW_PRESET, ndisparities, 11);
+    //!< sample code from https://github.com/Itseez/opencv/blob/52a785e95a30d9336bfbac97a0a0d0089ffaa7de/samples/cpp/stereo_match.cpp
+    //!< stereo matching object
+    cv::Ptr< cv::StereoBM > sbm = cv::createStereoBM( ndisparities, blockSize );
+    //cv::StereoBM SBM( cv::StereoBM::NARROW_PRESET, ndisparities, blockSize );
 #endif
 
 #ifdef USE_ELAS
     Elas::parameters param;
     param.postprocess_only_left = true;
-    param.disp_min = -64;
-    param.disp_max = 128;
+    param.disp_min = 0;
+    param.disp_max = ndisparities;
     const int32_t dims[3] = {640, 480, 640};
     Elas elas(param);
     //float* D1_data = (float*)malloc(640*480*sizeof(float));
@@ -216,30 +261,33 @@ int main( int /*argc*/, char** /*argv*/ )
 #endif
 
 
-    for ( int j = 0; j < k_numImages; j++ )
+    FlyCapture2::Image rawImage; //!< buffer
+
+    while ( 1 )
     {
-        // Display the timestamps for all cameras to show that the image
-        // capture is synchronized for each image
+        //!< Display the timestamps for all cameras to show that the image
+        //!< capture is synchronized for each image
 
-
+        sbm->setBlockSize( (blockSize + 2) * 2 + 1 );
+        sbm->setNumDisparities( (ndisparities+1) * 16 );
 
         for ( unsigned int i = 0; i < numCameras; i++ )
         {
-            FlyCapture2::Image rawImage;
+            FlyCapture2::Error error;
             error = ppCameras[i]->RetrieveBuffer( &rawImage );
             PRINT_ERROR( error );
 
-
-
             memcpy( pimageCamera[i]->data, rawImage.GetData(), rawImage.GetDataSize() );
-
-
         }
-        cv::remap( *pimageCamera[1], *pimageCamera[1], mx1, my1, CV_INTER_LINEAR );
-        cv::remap( *pimageCamera[0], *pimageCamera[0], mx2, my2, CV_INTER_LINEAR );
+
+        //!< rectifying images (and un-distorting?)
+        cv::remap( *pimageCamera[1], *pimageCamera[1], mx1, my1, cv::INTER_LINEAR );
+        cv::remap( *pimageCamera[0], *pimageCamera[0], mx2, my2, cv::INTER_LINEAR );
+
 
 #ifdef USE_OPENCV
-        SBM(*pimageCamera[1] , *pimageCamera[0] , imageDisparity, CV_16S);
+        sbm->compute( *pimageCamera[1], *pimageCamera[0], imageDisparity );
+        //SBM( *pimageCamera[1], *pimageCamera[0], imageDisparity, CV_16S );
 #endif
 
 #ifdef USE_ELAS
@@ -255,7 +303,7 @@ int main( int /*argc*/, char** /*argv*/ )
         minMaxLoc( imageDisparity, &minVal, &maxVal );
         imageDisparity.convertTo( disp8U, CV_8UC1, 255/(maxVal - minVal) );
 
-        imshow( "disparity", disp8U );
+        cv::imshow( "disparity", disp8U );
         cv::imshow( "cam0", *pimageCamera[1] );
         cv::imshow( "cam1", *pimageCamera[0] );
         cv::imshow( "sub",  cv::abs( *pimageCamera[0] - *pimageCamera[1] ) );
@@ -269,7 +317,7 @@ int main( int /*argc*/, char** /*argv*/ )
 
     }
 
-    exit(0); // hack; code below may freeze.
+    exit(0); //!< hack; code below may freeze.
 
     for ( unsigned int i = 0; i < numCameras; i++ )
     {
